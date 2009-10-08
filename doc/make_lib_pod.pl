a141662737a46d05ce6cea1f091d5b360704384a
Peter Karman <karpet@peknet.com>
2009-10-08 03:49:12 +0000

    build docs in different dir

--- doc/make_lib_pod.pl
+++ doc/make_lib_pod.pl
@@ -8,8 +8,7 @@
 use strict;
 use warnings;
 
-sub slurp
-{
+sub slurp {
     my $file = shift;
     local $/;
     open(F, "< $file") or die "can't open $file: $!";
@@ -18,8 +17,7 @@ sub slurp
     return $buf;
 }
 
-sub write_file
-{
+sub write_file {
     my ($file, $buf) = @_;
     open(F, "> $file") or die "can't write $file: $!";
     print F $buf;
@@ -37,8 +35,7 @@ sub write_file
 #
 # plus need to add a single space at ^ in order to verbatimize
 
-sub make_pod
-{
+sub make_pod {
     my $buf = shift;
     my @Pod;
     while ($buf =~ m!^/\*\s+(=head2 .*?\s+)\*/(.*?)/\*\s+=cut\s+\*/!gms)
@@ -51,8 +48,8 @@ sub make_pod
     return join("\n", @Pod);
 }
 
-my $header  = '../src/libswish3/libswish3.h';
-my $tmpl    = 'libswish3.3.pod.in';
+my $header  = shift(@ARGV) || '../src/libswish3/libswish3.h';
+my $tmpl    = shift(@ARGV) || 'libswish3.3.pod.in';
 my $pattern = '<<libswish3.h_HERE>>';           # must match .in file
 my $buf     = make_pod(slurp($header));
 my $pod = slurp($tmpl);
