7a56158f66dec92ca1134e36664d5397e5f479d1
Peter Karman <karpet@peknet.com>
2009-10-08 19:05:11 +0000

    rename utf8test to swish_utf8 for consistency

--- src/swish_utf8.c
+++ src/swish_utf8.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of libswish3
+ * Copyright (C) 2009 Peter Karman
+ *
+ *  libswish3 is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  libswish3 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libswish3; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 /* test utf8 functions */
 
 #include <stdio.h>
@@ -49,8 +68,8 @@ usage(
 )
 {
 
-    char *descr = "utf8test is an example program for testing the utf8 functions\n";
-    printf("utf8test [opts] [string(s)]\n");
+    char *descr = "swish_utf8 is an example program for testing the libswish3 utf8 functions\n";
+    printf("swish_utf8 [opts] [string(s)]\n");
     printf("opts:\n --file file.txt\n --debug\n");
     printf("\n%s\n\n", descr);
     exit(1);
