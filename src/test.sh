29a9d5f020f88654bc78984e09e67b1190358e77
Peter Karman <karpet@peknet.com>
2009-10-08 04:11:23 +0000

    set @INC explicitly too

--- src/test.sh
+++ src/test.sh
@@ -2,4 +2,4 @@
 if [ "$SVNDIR" == "" ]; then
     SVNDIR=..
 fi
-PERL_DL_NONLAZY=1 perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 't')" $SVNDIR/src/t/*.t
+PERL_DL_NONLAZY=1 perl -I$SVNDIR/src/t "-MExtUtils::Command::MM" "-e" "test_harness(0, 't')" $SVNDIR/src/t/*.t
