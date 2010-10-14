3f8d18760539a97c3b44c12e4d2332832de9adcd
Peter Karman <karpet@peknet.com>
2010-10-14 04:33:57 +0000

    fix conditional include of alloca.h

--- perl/cat-libswish3.pl
+++ perl/cat-libswish3.pl
@@ -67,7 +67,25 @@ my $includes = <<EOF;
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
-#include <alloca.h>
+
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif defined __GNUC__
+# define alloca __builtin_alloca
+#elif defined _AIX
+# define alloca __alloca
+#elif defined _MSC_VER
+# include <malloc.h>
+# define alloca _alloca
+#else
+# ifndef HAVE_ALLOCA
+#  ifdef  __cplusplus
+extern "C"
+#  endif
+void *alloca (size_t);
+# endif
+#endif
+
 #include <stdio.h>
 #include <locale.h>
 #include <stdarg.h>
